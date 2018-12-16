8a82c603183425a9a7fec34540d7e64268aacf07
HIMA <mjmjandk@naver.com>
2018-12-16 20:25:24 +0900

    1065

---
+++ algorithm/baekjoon/1065.py
@@ -0,0 +1,12 @@
+n = int(input())
+count = 0
+if n < 99:
+    print(n)
+elif n <= 110:
+    print(99)
+elif n <= 1000:
+    count = 99
+    for i in range(110, n + 1):
+        if int(str(i)[2]) - int(str(i)[1]) == int(str(i)[1]) - int(str(i)[0]):
+            count += 1
+    print(count)
