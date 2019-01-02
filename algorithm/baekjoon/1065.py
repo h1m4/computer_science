e2e247161791d53e34b9864b4b221af42d044e51
HIMA <mjmjandk@naver.com>
2019-01-02 15:28:10 +0900

    10809

--- algorithm/baekjoon/1065.py
+++ algorithm/baekjoon/1065.py
@@ -10,3 +10,5 @@ elif n <= 1000:
         if int(str(i)[2]) - int(str(i)[1]) == int(str(i)[1]) - int(str(i)[0]):
             count += 1
     print(count)
+
+# 입력받은 수와 같거나 작은 한수를 출력하는 줄 알고 삽질함...ㅎㅎ... 한수의 갯수를 출력하는 것이었다...
