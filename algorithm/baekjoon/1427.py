6121f5344a4c2307904cdfad420afc5ac604da2e
Minjeong Kang <mjmjandk@naver.com>
2019-02-28 12:17:28 +0900

    Update 1427.py

--- algorithm/baekjoon/1427.py
+++ algorithm/baekjoon/1427.py
@@ -1,6 +1,7 @@
 '''
 정렬 알고리즘 참고자료 :http://ejklike.github.io/2017/03/04/sorting-algorithms-with-python.html
-sorted() 함수는 입력값을 정렬한 후 그 결과를 리스트로 리턴하는 함수이다. 문자열이라도 괜찮음. 매우 편리
+sorted() 함수는 입력값을 정렬한 후 그 결과를 리스트로 리턴하는 함수이다. sort를 사용할 수 없는 이유는 sort는 리스트 자료형에만
+가능하기 때문 sorted는 
 '''
 
 print(''.join(reversed(sorted(input()))))
